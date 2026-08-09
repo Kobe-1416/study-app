7aead17a0607d8389c9b175067911337d86826ed
Kobe <irissneo@gmail.com>
2026-08-10 00:30:01 +0200

    add supabase connection setup, and test route to see if connection to db works

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
+const testRoutes = require("./routes/testRoutes");
 
 const app = express();
 const PORT = 5000;
+
+app.use(express.json());
+
+app.use("/api", testRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
